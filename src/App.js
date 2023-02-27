130564482fd552c41ba5832bd98e608db8f7102b
jktilavat <jktialvat7@gmail.com>
2023-02-27 08:09:44 +0530

    new module

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import PrimarySearchAppBar from "./components/SecondNav/PrimarySearchAppBar";
+import ButtonAppBar from "./components/FirstNavbar/FirstNav";
+import ThirdAppBar from "./components/ThirdNavbar/ThirdNav";
+import MainCarousel from "./components/Main Carousel/MainCarousel";
+import RowCarosel from "./components/RowCarousel/RowCarosel";
+import ErrPage from "./components/errPage/ErrPage";
+import MissinngCart from "./components/missing cart/MissinngCart";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ContactUs from "./components/ContactUs/ContactUs";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <ButtonAppBar />
+        {/* <PrimarySearchAppBar />
+        <ThirdAppBar /> */}
+        {/* <MainCarousel /> */}
+        <Routes>
+          {/* <Route index element={<ErrPage />}> */}
+          <Route path="/" element={<MainCarousel />}></Route>
+          <Route path="/missing-cart" element={<MissinngCart />}></Route>
+          <Route path="*" element={<ErrPage />}></Route>
+          <Route path="/contact-us" element={<ContactUs />}></Route>
+          {/* </Route> */}
+        </Routes>
+      </BrowserRouter>
+      <RowCarosel />
     </div>
   );
 }
